5ce495946a9429ae299ccd486eb64fae09759d9b
sandalu95 <sandalukalpanee@gmail.com>
2018-05-18 16:58:23 +0530

    custom server with multi client facility

--- multi/server/index.js
+++ multi/server/index.js
@@ -1,9 +1,13 @@
 var server = require('ws').Server;
-var s = new server({port:5002});
+var s = new server({port:5001});
 
 s.on('connection',function(ws){
 	ws.on('message',function(message){
 		console.log("Recieved : "+message);
 		ws.send("server sends : "+message);
 	});
-}); 
+	ws.on('close',function(){
+		console.log("I lost a client");
+	});
+	console.log("one more client connected");
+});
