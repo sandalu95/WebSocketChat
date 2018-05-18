022c16600b3be462a85c7760df0121963f34a986
sandalu95 <sandalukalpanee@gmail.com>
2018-05-18 19:25:24 +0530

    Chat Application completed version

--- server/index.js
+++
@@ -1,9 +0,0 @@
-var server = require('ws').Server;
-var s = new server({port:5002});
-
-s.on('connection',function(ws){
-	ws.on('message',function(message){
-		console.log("Recieved : "+message);
-		ws.send("server sends : "+message);
-	});
-}); 
